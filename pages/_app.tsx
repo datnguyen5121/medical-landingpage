2ce3be9fc439abefcd53f7687c910d8ebcf3b48d
nguyendangthanhdat <datnguyen5121@gmail.com>
2026-07-02 23:41:43 +0700

    Update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Script from 'next/script'
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
 
@@ -8,6 +9,18 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <link rel="icon" href="/favicon/newlogo.png" type="image/png" />
       </Head>
+
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=AW-18294272736"
+        strategy="afterInteractive"
+      />
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'AW-18294272736');`}
+      </Script>
+
       <Component {...pageProps} />
     </>
   )
